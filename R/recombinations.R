09c1ca424c546d581b3dc313efeaec777575433d
jona <yablee@gmail.com>
2017-10-09 19:14:03 +0200

    documentation for smooth.and.recombine

--- R/recombinations.R
+++ R/recombinations.R
@@ -14,9 +14,32 @@ project.from.network.recombine <- function(original_expression, smoothed_express
     return(data_in_original_space)
 }
 
-smooth.and.recombine <- function(gene_expression, adjnorm, lambda, smoothing.function=netsmooth::random.walk.by.iterations) {
-    gene_expression_in_A_space <- project.on.network(gene_expression, rownames(adjnorm))
-    gene_expression_in_A_space_smooth <- smoothing.function(gene_expression_in_A_space, adjnorm, lambda)
+#' Perform network smoothing on network when the network genes and the
+#' experiment genes aren't exactly the same.
+#'
+#' The gene network might be defined only on a subset of genes that are
+#' measured in any experiment. Further, an experiment might not measure all
+#' genes that are present in the network. This function projects the experiment
+#' data onto the gene space defined by the network prior to smoothing. Then,
+#' it projects the smoothed data back into the original dimansions.
+#'
+#' @param gene_expression  gene expession data to be smoothed. [N_genes x M_samples]
+#' @param adj_matrix  adjacenty matrix of network to perform smoothing over.
+#'                    Will be column-normalized.
+#'                    Rownames and colnames should be genes.
+#' @param lambda  network smoothing parameter (1 - restart probability in random
+#'                walk model.
+#' @param smoothing.function  must be a function that takes in data, adjacency
+#'                            matrix, and lambda. Will be used to perform the
+#'                            actual smoothing.
+#' @usage  smooth.and.recombine(gene_expression, adj_matrix, lambda,
+#'                              smoothing.function=netsmooth::smooth.by.matrix.inv)
+#' @return  matrix with network-smoothed gene expression data. Genes that are
+#'          not present in smoothing network will retain original values.
+#' @export
+smooth.and.recombine <- function(gene_expression, adj_matrix, lambda, smoothing.function=netsmooth::random.walk.by.iterations) {
+    gene_expression_in_A_space <- project.on.network(gene_expression, rownames(adj_matrix))
+    gene_expression_in_A_space_smooth <- smoothing.function(gene_expression_in_A_space, adj_matrix, lambda)
     gene_expression_smooth <- project.from.network.recombine(gene_expression, gene_expression_in_A_space_smooth)
     return(gene_expression_smooth)
 }
