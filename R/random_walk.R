6f96d0a1631a3aa61b50036a334868b5c5667acc
jona <yablee@gmail.com>
2017-11-26 21:20:13 +0100

    implement solve() walker

--- R/random_walk.R
+++ R/random_walk.R
@@ -24,20 +24,40 @@ l1NormalizeColumns <- function(A) {
 #' distribution of the random walk with restarts process.
 #'
 #' The closed-form solution is given by
-#'   f_{ss} = (1 - lambda) * (I - lambda * A)^{-1} * f_0
+#'   f_{ss} = (1 - alpha) * (I - alpha * A)^{-1} * f_0
 #' and is computed by matrix inversion in this function.
 #'
 #' @param f0      initial data matrix [NxM]
 #' @param adj_matrix       adjacency matrix of graph to network smooth on
 #'                will be column-normalized.
-#' @param lambda  smoothing coefficient (1 - restart probability of
+#' @param alpha  smoothing coefficient (1 - restart probability of
 #'                random walk)
-#' @usage randomWalkByMatrixInv(gene_expression, adj_matrix, lambda)
+#' @usage randomWalkByMatrixInv(gene_expression, adj_matrix, alpha)
 #' @return network-smoothed gene expression
 #' @export
-randomWalkByMatrixInv <- function(f0, A, lambda) {
+randomWalkByMatrixInv <- function(f0, A, alpha) {
     Anorm <- l1NormalizeColumns(A)
     eye <- diag(dim(A)[1])
-    K <- (1 - lambda) * solve(eye - lambda * Anorm)
+    K <- (1 - alpha) * solve(eye - alpha * Anorm)
     return(K %*% f0)
+}
+
+#' Smooth data on graph by solving the linear equation
+#' (I - alpha*A)^T * E_sm^T = E^T * (1-alpha)
+#'
+#'
+#' @param E      initial data matrix [NxM]
+#' @param A      adjacency matrix of graph to network smooth on
+#'               will be column-normalized.
+#' @param alpha  smoothing coefficient (1 - restart probability of
+#'                random walk)
+#' @return network-smoothed gene expression
+#' @export
+randomWalkBySolve <- function(E, A, alpha) {
+    E <- t(E)
+    Anorm <- t(t(A) / colSums(A))
+    eye <- diag(dim(A)[1])
+    AA <- t(eye - alpha*Anorm)
+    BB <- (1-alpha) * t(E)
+    return(t(solve(AA, BB)))
 }
