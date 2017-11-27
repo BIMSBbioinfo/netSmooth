ef13ab3b6c154c79300ac4304b921aaa16cf3029
jona <yablee@gmail.com>
2017-11-27 22:03:44 +0100

    more tests for recombinations

--- tests/testthat/test_recombinations.R
+++ tests/testthat/test_recombinations.R
@@ -1,7 +1,5 @@
 context("Recombinations")
 
-# projectOnNetwork(gene_expression, new_features, missing.value=0)
-
 test_that("projectOnNetwork can subset", {
     expr <- matrix(rep(1,20), ncol=5)
     rownames(expr) <- paste('gene', 1:(dim(expr)[1]))
@@ -29,4 +27,52 @@ test_that("projectOnNetwork fills missing values", {
 
     projected <- projectOnNetwork(expr, network.genes, missing.value = NA)
     expect_equal(projected['gene 5',], as.numeric(rep(NA, dim(expr)[2])))
+})
+
+############################
+
+expr <- matrix(rep(1,20), ncol=5)
+rownames(expr) <- paste('gene', 1:(dim(expr)[1]))
+
+expr_sm <- matrix(rep(2,30), ncol=5)
+rownames(expr_sm) <- paste('gene', 2:(dim(expr_sm)[1]+1))
+
+expr_sm_projected_back <- projectFromNetworkRecombine(expr, expr_sm)
+
+test_that("projectFromNetworkRecombine uses space of original expression", {
+    expect_equal(dim(expr), dim(expr_sm_projected_back))
+})
+
+test_that("projectFromNetworkRecombine keeps original values", {
+    not_smoothed_genes <- setdiff(rownames(expr), rownames(expr_sm))
+    expect_equal(expr_sm_projected_back[not_smoothed_genes,],
+                 expr[not_smoothed_genes,])
+})
+
+test_that("projectFromNetworkRecombine uses smoothed values", {
+    smoothed_genes <- intersect(rownames(expr), rownames(expr_sm))
+    expect_equal(expr_sm_projected_back[smoothed_genes,],
+                 expr_sm[smoothed_genes,])
+})
+
+############################
+
+smoother <- function(expr, adjmatrix, alpha) expr+alpha
+expr <- matrix(rep(1,20), ncol=5)
+rownames(expr) <- paste('gene', 1:(dim(expr)[1]))
+
+adj <- matrix(rep(1,36), ncol=6)
+rownames(adj) <- colnames(adj) <- paste('gene', 2:(dim(adj)[1]+1))
+
+expr_sm <- smoothAndRecombine(expr, adj, .1, smoother)
+
+test_that("smoothAndRecombine smoothes genes in network", {
+    smoothed_genes <- intersect(rownames(expr), rownames(adj))
+    expect_false(isTRUE(all.equal(expr_sm[smoothed_genes,],
+                                  expr[smoothed_genes,])))
+})
+
+test_that("smoothAndRecombine leaves unsmoothed genes as is", {
+    not_smoothed_genes <- setdiff(rownames(expr), rownames(adj))
+    expect_equal(expr_sm[not_smoothed_genes,], expr[not_smoothed_genes,])
 })
