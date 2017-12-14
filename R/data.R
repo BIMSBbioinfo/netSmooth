4a332ea29e941a8df7e5e89dcf2cbc93e36cc334
jona <yablee@gmail.com>
2017-12-14 15:26:27 +0100

    add missing documentation for data objects

--- R/data.R
+++ R/data.R
@@ -15,3 +15,16 @@
 #' @format A square matrix where A_{ij}=1 if gene i interacts with gene j
 #' @source \url{http://www.string-db.org/}
 "mouse.ppi"
+
+#' A small single cell RNA-seq dataset for use in examples.
+#'
+#' Contains scRNAseq profiles of human blastomeres.
+#'
+#' @format SingleCellExperiment
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE44183}
+"smallscRNAseq"
+
+#' A small human Protein-Protein interaction graph for use in examples.
+#'
+#' Contains a synthetic PPI of human genes.
+"smallPPI"
