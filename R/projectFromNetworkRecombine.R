348894e9f7a8d9c9a30909c41dc21d1690e70a7c
jona <yablee@gmail.com>
2019-01-09 15:18:06 +0100

    fix global name note

--- R/projectFromNetworkRecombine.R
+++ R/projectFromNetworkRecombine.R
@@ -11,7 +11,7 @@ setGeneric(
 #' @param original_expression    the non-smoothed expression
 #' @param smoothed_expression    the smoothed gene expression, in the space
 #'                               of the genes defined by the network
-#' @param filepath      String: Path to location where hdf5 output file is supposed to be saved. 
+#' @param filepath      String: Path to location where hdf5 output file is supposed to be saved.
 #'                      Will be ignored when regular matrices or SummarizedExperiment are
 #'                      used as input.
 #' @return a matrix in the dimensions of original_expression, where values that
@@ -38,7 +38,7 @@ setMethod("projectFromNetworkRecombine",
                                        rownames(smoothed_expression))
             data_in_original_space[genes_in_both,] <- as.matrix(
               smoothed_expression[genes_in_both,])
-            
+
             return(data_in_original_space)
           })
 
@@ -48,26 +48,26 @@ setMethod("projectFromNetworkRecombine",
           function(original_expression,
                    smoothed_expression,
                    filepath = NULL) {
-            
-            
+
+
             # genes in orignal and smoothed expression
             genes.in.both <- intersect(original_expression@seed@dimnames[[1]],
                       smoothed_expression@seed@dimnames[[1]])
-            
+
             # genes not in intersection
             original.exclusive <- original_expression@seed@dimnames[[1]][which(!(original_expression@seed@dimnames[[1]] %in% genes.in.both))]
-            
+
             # use orignal expression as seed
-            data_in_original_space <- writeHDF5Array(DelayedArray(original_expression), filepath = filepath)
-            
+            data_in_original_space <- writeHDF5Array(DelayedArray::DelayedArray(original_expression), filepath = filepath)
+
             # set and row/col names and coerce to DelayedMatrix
             rownames(data_in_original_space) <- original_expression@seed@dimnames[[1]]
             colnames(data_in_original_space) <- original_expression@seed@dimnames[[2]]
-            
+
             # replace rows in output
             data_in_original_space[genes.in.both,] <- smoothed_expression[genes.in.both,]
 
             return(data_in_original_space)
           })
-  
+
 
